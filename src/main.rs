c3a7391b7eb065896f4690e653a11d035e722d36
LeeShinYeoung <iqrash12@gmail.com>
2023-09-12 23:28:52 +0900

    Implement tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+trait TcpConnectionStrategy {
+    fn handle(&self, stream: TcpStream);
+}
+
+struct ConnectionPool;
+
+impl TcpConnectionStrategy for ConnectionPool {
+    fn handle(&self, mut stream: TcpStream) {
+        loop {
+            let mut buffer = [0; 512];
+            stream.read(&mut buffer).unwrap();
+            println!("{}", String::from_utf8_lossy(&buffer));
+            stream.write(&buffer).unwrap();
+        }
+    }
+}
+
+struct TcpServerConfig {
+    strategy: Box<dyn TcpConnectionStrategy>
+}
+
+struct TcpServer {
+    config: TcpServerConfig
+}
+
+impl TcpServer {
+    fn new(config: TcpServerConfig) -> TcpServer {
+        TcpServer {
+            config
+        }
+    }
+
+    fn listen(&self, port: u16) -> std::io::Result<()> {
+        let host = "localhost";
+        let address = format!("{}:{}", host, port);
+        let listener = TcpListener::bind(address)?;
+
+        for stream in listener.incoming() {
+            self.config.strategy.handle(stream?);
+        }
+
+        Ok(())
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpServer::new(TcpServerConfig {
+        strategy: Box::new(ConnectionPool)
+    });
+
+    server.listen(8080).unwrap();
 }

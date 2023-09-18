b43f770df74371408380670f87ad7aac4e0e9556
LeeShinYeoung <iqrash12@gmail.com>
2023-09-18 21:32:42 +0900

    Implement connect pool

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,55 @@
 use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::net::TcpListener;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 trait TcpConnectionStrategy {
-    fn handle(&self, stream: TcpStream);
+    fn handle(&self, listener: TcpListener);
 }
 
 struct ConnectionPool;
 
 impl TcpConnectionStrategy for ConnectionPool {
-    fn handle(&self, mut stream: TcpStream) {
-        loop {
-            let mut buffer = [0; 512];
-            stream.read(&mut buffer).unwrap();
-            println!("{}", String::from_utf8_lossy(&buffer));
-            stream.write(&buffer).unwrap();
+    fn handle(&self, listener: TcpListener) {
+
+        let connection = Arc::new(Mutex::new(0));
+
+        for stream in listener.incoming() {
+            let connection = Arc::clone(&connection);
+
+            {
+                let mut connection = connection.lock().unwrap();
+                if *connection > 1 {
+                    println!("Connection #{} rejected", connection);
+                    continue;
+                }
+
+                *connection += 1;
+            }
+
+            let mut stream = stream.unwrap();
+
+            thread::spawn(move || {
+                loop {
+                    let mut buffer = [0; 512];
+                    let byte = stream.read(&mut buffer).unwrap();
+
+                    if byte == 0 {
+                        let mut connection = connection.lock().unwrap();
+                        println!("Connection #{} closed", connection);
+                        *connection -= 1;
+                        break;
+                    }
+
+                    println!("{}", String::from_utf8_lossy(&buffer));
+                    stream.write(&buffer).unwrap();
+
+                    {
+                        let connection = connection.lock().unwrap();
+                        println!("Connection #{}", connection);
+                    }
+                }
+            });
         }
     }
 }
@@ -37,10 +73,7 @@ impl TcpServer {
         let host = "localhost";
         let address = format!("{}:{}", host, port);
         let listener = TcpListener::bind(address)?;
-
-        for stream in listener.incoming() {
-            self.config.strategy.handle(stream?);
-        }
+        self.config.strategy.handle(listener);
 
         Ok(())
     }
